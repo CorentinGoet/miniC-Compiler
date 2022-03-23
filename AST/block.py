8c7f67dcd19f0bd81ee8c8d1402052a1c1a284f0
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2022-03-23 23:02:41 +0100

    added block

---
+++ AST/block.py
@@ -0,0 +1,27 @@
+"""
+@author Corentin Goetghebeur (github.com/CorentinGoet)
+"""
+
+from AST.node import Node
+
+
+class Block(Node):
+    """
+    Class representation for the Block node.
+
+    The Block node has the following syntax:
+
+    Block = {Statements}
+    """
+
+    def __init__(self, statements: list = None):
+        """
+        Constructor for the Block node.
+
+        :param statements: list of Statements nodes
+        """
+        self.statements= statements
+
+    def accept(self, visitor):
+        visitor.visitBlock()
+

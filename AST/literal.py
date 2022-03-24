ff4662916587d4f5d38bcd996f8279c54ba9fff9
Corentin goetghebeur <corentin.goetghebeur@ensta-bretagne.org>
2022-03-24 17:52:33 +0100

    added literal

---
+++ AST/literal.py
@@ -0,0 +1,29 @@
+"""
+@author Corentin Goetghebeur (github.com/CorentinGoet)
+"""
+
+from AST.node import Node
+import enum
+
+
+class Literal(Node):
+    """
+    Class representation for the Literal node.
+
+    The literal node has the following syntax:
+
+    Literal = Integer | Boolean | Float | Char
+    """
+
+    def __init__(self, sonNode):
+        """
+        Constructor for the literal node.
+
+        :param sonNode: Integer or Boolean or Float or Char
+        """
+        self.sonNode = sonNode
+
+    def accept(self, visitor):
+        visitor.visitLiteral()
+
+

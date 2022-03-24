0de5911bdfee53da20962d7ccba285d9ee87d9e9
Corentin goetghebeur <corentin.goetghebeur@ensta-bretagne.org>
2022-03-24 17:48:12 +0100

    added primary

---
+++ AST/primary.py
@@ -0,0 +1,28 @@
+"""
+@author Corentin Goetghebeur (github.com/CorentinGoet)
+"""
+
+from AST.node import Node
+
+
+class Primary(Node):
+    """
+    Class representation for the Primary node.
+
+    Primary node has the following syntax:
+
+    Primary = Identifier [ [Expression] ] | Literal | ( Expression ) | Type (Expression)
+    """
+
+    def __init__(self, sonNode):
+        """
+        Constructor for the primary node.
+
+        :param sonNode: Identifier or Literal or (Expression) or Type + Expression
+        """
+        self.sonNode = sonNode
+
+    def accept(self, visitor):
+        visitor.visitPrimary()
+
+
